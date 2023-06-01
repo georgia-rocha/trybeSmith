dfa4276b77332ccd9a4fe34e6811b5788e8e97f9
Georgia <georgiab543@gmail.com>
2023-06-01 10:02:36 -0300

    feat: requisito 3 s/ testes

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,19 +1,24 @@
+import { Order } from '../types/Order';
 import ProductModel from '../database/models/product.model';
-import OrderModel, { OrderSequelizeModel } from '../database/models/order.model';
+import OrderModel from '../database/models/order.model';
 import { ServiceResponse } from '../types/ServiceResponse';
 
-async function getOrdersAll(): Promise<ServiceResponse<OrderSequelizeModel[]>> {
+async function getOrdersAll(): Promise<ServiceResponse<Order[]>> {
   const orders = await OrderModel.findAll({ include: {
     model: ProductModel,
     as: 'productIds',
     attributes: ['id'],
   } });
-
-  orders.forEach((o) => {
-    console.log(o.dataValues.productId);
+ 
+  const response = orders.map((order) => {
+    const productIds = order.dataValues.productIds?.map((o) => (o as unknown as Order).id).sort();
+    return {
+      id: order.dataValues.id,
+      userId: order.dataValues.userId,
+      productIds,
+    };
   });
-
-  return { status: 'SUCCESSFUL', data: orders };
+  return { status: 'SUCCESSFUL', data: response };
 }
 
 export default {
