f5634fba49837719cd99581c186c8e53a173744c
Georgia <georgiab543@gmail.com>
2023-06-01 11:27:03 -0300

    feat: adicionando teste para casos de erro de getProducts

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -9,7 +9,6 @@ async function getOrdersAll(): Promise<ServiceResponse<Order[]>> {
     as: 'productIds',
     attributes: ['id'],
   } });
-  // console.log(orders);
  
   const response = orders.map((order) => {
     const productIds = order.dataValues.productIds?.map((o) => (o as unknown as Order).id).sort();
